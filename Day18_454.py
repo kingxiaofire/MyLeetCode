6d637aa3d39802795ab9cd86eb2df3dc3c9c80c8
kingxiaofire <1179271336@qq.com>
2023-09-02 21:41:59 +0800

    Day18

---
+++ Day18_454.py
@@ -0,0 +1,17 @@
+class Solution(object):
+    def fourSumCount(self, nums1, nums2, nums3, nums4):
+        hashmap = dict()
+        for n1 in nums1:
+            for n2 in nums2:
+                if n1 + n2 in hashmap:
+                    hashmap[n1+n2] += 1
+                else:
+                    hashmap[n1+n2] = 1
+            
+        count = 0
+        for n3 in nums3:
+            for n4 in nums4:
+                key = -n3 - n4
+                if key in hashmap:
+                    count += hashmap[key]
+        return count
