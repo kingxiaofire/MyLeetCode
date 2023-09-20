59d70043304ee4fbad50938cba3ccc7e22f997cc
kingxiaofire <1179271336@qq.com>
2023-09-20 09:38:28 +0800

    Day31

---
+++ Day31_1047.py
@@ -0,0 +1,9 @@
+class Solution:
+    def removeDuplicates(self, s: str) -> str:
+        res = list()
+        for item in s:
+            if res and res[-1] == item:
+                res.pop()
+            else:
+                res.append(item)
+        return "".join(res)
